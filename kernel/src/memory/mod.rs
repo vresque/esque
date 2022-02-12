5b5ce8b6a4f857295fbf99539fcb85e6286c6580
schuetzschab <panemlp@gmx.net>
2022-02-12 18:07:27 +0100

    [feat] Added PCI Controller

--- kernel/src/memory/mod.rs
+++ kernel/src/memory/mod.rs
@@ -11,3 +11,30 @@ macro_rules! address_of {
         ($x) as *const _ as *const u64 as u64
     };
 }
+
+#[macro_export]
+/// # From Address
+/// Gets a reference from an address
+/// ## Possible Invokations
+/// ```
+/// // The following two expressions are equal
+/// let x: &MyType = from_addr!(0xFF23f34)
+/// let y = from_addr!(MyType: 0xFF23f34)
+/// // So are the next two
+/// let mut_x: &mut MyType = from_addr!(mut 0xFF23f34)
+/// let mut_y = from_addr!(mut MyType: 0xFF23f34)
+/// ```
+macro_rules! from_addr {
+    ($a:expr) => {
+        (unsafe { &*(($a) as *const u64 as *const _) })
+    };
+    ($ty:ty: $a:expr) => {
+        (unsafe { &*(($a) as *const u64 as *const $ty) })
+    };
+    (mut $ty:ty: $a:expr) => {
+        (unsafe { &mut *(($a) as *mut u64 as *mut $ty) })
+    };
+    (mut $a:expr) => {
+        (unsafe { &mut *(($a) as *mut u64 as *mut _) })
+    };
+}
--- kernel/src/pci/mod.rs
+++ kernel/src/pci/mod.rs
@@ -2,8 +2,24 @@ use core::mem::size_of;
 
 use crate::{
     acpi::{config::DeviceConfig, ACPITable, MCFGHeader},
-    address_of,
+    address_of, debug, from_addr,
+    memory::paging::page_table_manager::PAGE_TABLE_MANAGER,
 };
+
+struct PCIDeviceHeader {
+    pub vendor_id: u16,
+    pub device_id: u16,
+    pub command: u16,
+    pub status: u16,
+    pub revision_id: u8,
+    pub program_interface: u8,
+    pub subclass: u8,
+    pub class: u8,
+    pub cache_line_size: u8,
+    pub latency_timer: u8,
+    pub header_type: u8,
+    pub bist: u8,
+}
 
 pub struct PCI {}
 
@@ -24,16 +40,74 @@ impl PCI {
 
             // Iterate over all of the buses
             for bus in config.start_bus..config.end_bus {
-                self.enumerate_bus(config.base, bus as i64);
+                self.enumerate_bus(config.base, bus as u64);
             }
         }
     }
 
-    fn enumerate_bus(&self, base: u64, bus: i64) {
+    fn enumerate_bus(&self, base: u64, bus: u64) {
         let offset = bus << 20;
-        let _address = base as i64 + offset; //  The Address of the bus
-    }
-    fn enumerate_device(&self, _base: u64, _device: u64) {}
+        let address = base + offset; //  The Address of the bus
+        unsafe {
+            PAGE_TABLE_MANAGER
+                .lock()
+                .assume_init_mut()
+                .map_memory(address as u64, address as u64);
+        }
 
-    fn enumerate_function(&self, _device_addr: u64, _func: u64) {}
+        let header: &PCIDeviceHeader = from_addr!(address);
+
+        // Check for invalid Device IDs
+        if header.device_id == 0 || header.device_id == 0xFFFF {
+            return;
+        }
+
+        // 32 Devices per Bus
+        for device in 0..32 {
+            self.enumerate_device(address, device)
+        }
+    }
+    fn enumerate_device(&self, bus_address: u64, device: u64) {
+        let offset = device << 15;
+        let address = bus_address + offset; //  The Address of the bus
+        unsafe {
+            PAGE_TABLE_MANAGER
+                .lock()
+                .assume_init_mut()
+                .map_memory(address as u64, address as u64);
+        }
+
+        let header: &PCIDeviceHeader = from_addr!(address);
+
+        // Check for invalid Device IDs
+        if header.device_id == 0 || header.device_id == 0xFFFF {
+            return;
+        }
+        // 8 Functions per Device
+        for function in 0..8 {
+            self.enumerate_function(address, function)
+        }
+    }
+
+    fn enumerate_function(&self, device_addr: u64, func: u64) {
+        let offset = func << 12;
+        let address = device_addr + offset; //  The Address of the bus
+        unsafe {
+            PAGE_TABLE_MANAGER
+                .lock()
+                .assume_init_mut()
+                .map_memory(address as u64, address as u64);
+        }
+
+        let header: &PCIDeviceHeader = from_addr!(address);
+
+        // Check for invalid Device IDs
+        if header.device_id == 0 || header.device_id == 0xFFFF {
+            return;
+        }
+
+        // Print all PCI Devices
+        debug!("Vendor ID: {:#x?}", header.vendor_id);
+        debug!("Device ID: {:#x?}", header.device_id);
+    }
 }
