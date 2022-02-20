e3f4e3fb82a4c22759bd8aa1a1617ed06947be81
schuetzschab <panemlp@gmx.net>
2022-02-20 17:52:08 +0100

    [fix] Fixed missing keys in the CI.toml
    I added these missing keys yesterday and forgot to include them in the
    CI config file

--- scripts/cloc.sh
+++ scripts/cloc.sh
@@ -1,2 +1,6 @@
-set -e
-find ./ -name "*.rs" -print0 | xargs -0 cat | wc -l
+#!/bin/bash
+RUSTNUM=`find ./ -name "*.rs" -print0 | xargs -0 cat | wc -l | sed 's/\r$//'`
+DOCNUM=`find ./ -name "*.md" -print0 | xargs -0 cat | wc -l | sed 's/\r$//'`
+PYNUM=`find ./ -name "*.py" -print0 | xargs -0 cat | wc -l | sed 's/\r$//'`
+ALLNUM=`expr $PYNUM + $RUSTNUM + $DOCNUM`
+printf "There are %u lines of rust, $PYNUM lines of python and $DOCNUM lines of documentation." $RUSTNUM
--- scripts/unsafe-counter.sh
+++ scripts/unsafe-counter.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 unsafes=`grep -Rnw --include=\*.rs -e "unsafe" | wc -l`
 rustlines=`find ./ -name "*.rs" -print0 | xargs -0 cat  | wc -l`
 ratio=`echo "scale=5 ; $unsafes / $rustlines" | bc`
